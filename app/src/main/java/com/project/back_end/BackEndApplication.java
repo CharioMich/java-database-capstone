d3bcc717f6d044ee0ac8895271d8219fa3f5e66c
Ch. Michalakis <mpampisb@gmail.com>
2025-11-05 14:11:36 +0200

    feat: add @EnableJpaRepositories annotation on app entry point class

--- app/src/main/java/com/project/back_end/BackEndApplication.java
+++ app/src/main/java/com/project/back_end/BackEndApplication.java
@@ -3,9 +3,11 @@ package com.project.back_end;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan("com.project.back_end")
+@EnableJpaRepositories("com.project.back_end.repo")
 public class BackEndApplication {
 
 	public static void main(String[] args) {
